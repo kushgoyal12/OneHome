c96f8ce0c75232d6a43e78edc8709213131c56ec
Lakshay-812 <lj604@snu.edu.in>
2022-04-24 16:35:22 +0530

    further changes on landing page; volunteer display

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,4 +256,23 @@ function reveal() {
   }
 }
 
+
+function reveal_1() {
+  var reveals = document.querySelectorAll(".more-services");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
 window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", reveal_1);
+

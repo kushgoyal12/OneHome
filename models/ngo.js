b19aee31710b9f3c3215f56d8969c92a9e914847
Kush Goyal <kushgoyal09@gmail.com>
2022-04-25 12:55:23 +0530

    new changes

--- models/ngo.js
+++ models/ngo.js
@@ -4,9 +4,16 @@ const Schema = mongoose.Schema;
 const NgoSchema = new Schema({
     username: String,
     ngo_name: String,
-    location: String,
     about: String,
     email: String,
+    link: String,
+    street: String,
+    additional: String, 
+    zip: String,
+    place: String,
+    country: String, 
+    code: String,
+    phone: String,
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
--- routes/ngos.js
+++ routes/ngos.js
@@ -5,6 +5,10 @@ const { isLoggedIn } = require('../middleware');
 const catchAsync = require('../utilities/catchAsync');
 const Ngo = require("../models/ngo")
 
+router.get('/', isLoggedIn, catchAsync(async (req, res) => {
+    res.render('ngos/home')
+}))
+
 router.get('/v', (req, res) => {
     res.render('ngos/base')
 })
@@ -17,8 +21,21 @@ router.get('/register', (req, res) => {
     res.render('ngos/register')
 })
 
-router.get('/', isLoggedIn, catchAsync(async (req, res) => {
-    res.render('ngos/home')
+router.get('/profile', isLoggedIn, catchAsync(async(req, res) => {
+    const id = req.user._id;
+    let ngo = await Ngo.find({"author" : id});
+    ngo = ngo[0];
+    res.render('ngos/profile', {ngo});
 }))
+
+router.put('/', isLoggedIn, catchAsync(async(req, res) => {
+    const id = req.user._id;
+    let v = await Ngo.find({"author" : id});
+    v = v[0];
+    const vid = v._id;
+    const ngo = await Ngo.findByIdAndUpdate(vid, req.body, { runValidators: true, new: true });
+    res.redirect('/ngo');
+}))
+
 
 module.exports = router;
